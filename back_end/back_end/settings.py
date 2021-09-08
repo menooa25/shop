2a396a9e51d6c469cde8e1a275852626511a2148
menooa25 <menooa2013@gmail.com>
2021-09-09 00:14:12 +0430

    product modellig complited

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-x$)wtdf*jn=84jd!99%c9v11@33-(q2#$8ra7+8^40xclj42hl
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -77,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'back_end.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -91,7 +88,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -111,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -125,7 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -135,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# media
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- back_end/product/models.py
+++ back_end/product/models.py
@@ -4,13 +4,50 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'دسته‌بندی'
+        verbose_name_plural = 'دسته‌بندی ها'
+
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     name = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f'{self.name} -- {self.price} -- {self.quantity}'
+
+    class Meta:
+        verbose_name = 'محصول'
+        verbose_name_plural = 'محصولات'
+
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='image')
+    # product can have multiple images and main Image
+    is_primary = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='product/')
+
+    def __str__(self):
+        return f'{self.product} -- {self.is_primary}'
+
+    class Meta:
+        verbose_name = 'عکس'
+        verbose_name_plural = 'عکس ها'
+
 
 class Discount(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='discount')
     percent = models.PositiveIntegerField()
+    expire_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.product} -- {self.percent}'
+
+    class Meta:
+        verbose_name = 'کد تخفیف'
+        verbose_name_plural = 'کد های تخفیف'
