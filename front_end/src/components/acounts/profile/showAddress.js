11f699ff3b7190094cd488c16b89d95dac03bbc0
menooa25 <menooa2013@gmail.com>
2021-09-17 23:35:53 +0430

    dynamic changing address button

--- front_end/src/components/acounts/profile/showAddress.js
+++ front_end/src/components/acounts/profile/showAddress.js
@@ -10,7 +10,8 @@ class ShowAddress extends Component {
           to="/profile/address"
           className="ml-auto m-2 btn h-25 btn-outline-info btn-sm "
         >
-          ویرایش آدرس
+            {this.props.address &&' ویرایش آدرس'}
+            {!this.props.address &&'افزودن آدرس'}
         </Link>
       </div>
     );
