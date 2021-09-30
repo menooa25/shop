bbd9d4ba831e46da1bfa63df9e42610f0df788cc
menooa25 <menooa2013@gmail.com>
2021-09-30 21:43:47 +0330

    project is almost done

--- back_end/order/api/serializers.py
+++ back_end/order/api/serializers.py
@@ -19,6 +19,23 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class OrderSimpleSerializer(serializers.ModelSerializer):
+
+    def validate(self, attrs):
+        super().validate(attrs)
+        product_quantity = attrs['product'].quantity
+        quantity = attrs['quantity']
+        if product_quantity < quantity:
+            raise serializers.ValidationError('there is not enough product to parches ')
+        return attrs
+
+    def save(self, **kwargs):
+        super().save(**kwargs)
+        product_id = self.data.get('product')
+        product = Product.objects.get(id=product_id)
+        product.quantity -= self.data.get('quantity')
+        product.save()
+        product.save()
+
     class Meta:
         model = Order
         fields = '__all__'
